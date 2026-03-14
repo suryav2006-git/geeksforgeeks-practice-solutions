85baa781d6096bbf86d132c4fd1afd1e6e50bb98
Surya V <mrstudieshelper@gmail.com>
2026-03-15 00:39:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/If conditional statement- Python/if-conditional-statement-python.py
@@ -0,0 +1,10 @@
+def friends_in_trouble(j_angry, s_angry):
+    
+    if (j_angry == True ) and (s_angry == True) :
+        return True
+        
+    elif (j_angry == False ) and (s_angry == False) :
+        return True
+    
+    elif (j_angry==False) or (s_angry==False) :
+        return False
