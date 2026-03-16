1cfcc976fd41bfd4cfe5741cbdaff82ca4d3e938
Surya V <mrstudieshelper@gmail.com>
2026-03-16 19:24:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Zero Converter - Python/zero-converter-python.py
@@ -0,0 +1,27 @@
+def pos(n):
+    ## Write the code
+    if n==0:
+        print("already Zero")
+        return
+    
+    while (n>0):
+        print(n-1,end=" ")
+        n-=1
+        
+    
+def neg(n):
+    ##Write the code
+    
+    if n==0 :
+        print("already Zero")
+        return
+    
+    while (n<=0):
+        print(n,end = " ")
+        n+=1
+        
+        
+        
+        
+        
+    
