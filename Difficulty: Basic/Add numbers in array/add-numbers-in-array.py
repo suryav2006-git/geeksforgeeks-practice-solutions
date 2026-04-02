f53623b01b8b8a3b9ec4fcfc68640724d53d3de9
Surya V <mrstudieshelper@gmail.com>
2026-04-02 09:54:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Add numbers in array/add-numbers-in-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def get_Sum(self, n, input):
+        # code here
+        sum = 0
+        for i in range(n) :
+            sum = sum + input[i]
+            
+        return sum
+        
