43e29cdf29671b0204e76af474d63e8959fb35b5
Surya V <mrstudieshelper@gmail.com>
2026-06-04 23:54:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Arithmetic Operators/arithmetic-operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    // code here
+    int p = a + b;
+    int q = a - b;
+    int r = a * b;
+    int s = a / b;
+    int t = a % b;
+
+    // The below code prints the output
+    cout << p << " " << q << " " << r << " " << s << " " << t << endl;
+    return 0;
+}
