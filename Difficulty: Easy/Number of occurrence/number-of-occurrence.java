9967cf330cbee16e048b45fbdb71d2c534206895
Surya V <mrstudieshelper@gmail.com>
2026-06-12 19:26:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.java
@@ -0,0 +1,55 @@
+class Solution {
+    
+    int FirstSearch(int[] nums, int n, int target) {
+        int low = 0;
+        int high = n-1;
+        int first = -1;
+        
+        while(low<=high) {
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target) {
+                first = mid;
+                high = mid - 1;
+            }
+            else if (nums[mid] >= target) {
+                high = mid -1 ;
+            }
+            else low = mid + 1;
+        }
+        
+        return first;
+    }
+    
+    int SecondSearch(int[] nums, int n, int target) {
+        int low = 0;
+        int high = n-1;
+        int second = -1;
+        
+        while(low<=high) {
+            int mid = low + (high - low ) / 2;
+            if(nums[mid] == target) {
+                second = mid;
+                low = mid + 1;
+            }
+            else if (nums[mid] > target) {
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+        return second;
+    }
+    
+    int countFreq(int[] nums, int target) {
+        // code here
+        int n = nums.length;
+        int first = -1;
+        int second = -1;
+        
+        first = FirstSearch(nums,n,target);
+        if(first == -1) return 0;
+        
+        second = SecondSearch(nums,n,target);
+        
+        return second - first + 1;
+    }
+}
