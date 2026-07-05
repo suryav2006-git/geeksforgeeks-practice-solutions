731077d7bd726af6ed12415ec094b116907b3058
Surya V <mrstudieshelper@gmail.com>
2026-07-05 23:21:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Median of 2 Sorted Arrays of Different Sizes/median-of-2-sorted-arrays-of-different-sizes.java
+++ Difficulty: Hard/Median of 2 Sorted Arrays of Different Sizes/median-of-2-sorted-arrays-of-different-sizes.java
@@ -1,64 +1,46 @@
 class Solution {
-    public double medianOf2(int a[], int b[]) {
-        // Code Here
-        int n1 = a.length;
+
+    public int max(int a, int b ) {
+        if(a>=b) return a;
+        else return b;
+    }
+
+    public int min(int a, int b ) {
+        if(a<=b) return a;
+        else return b;
+    }
+
+    public double medianOf2(int[] a, int[] b) {
+        int n1 =  a.length;
         int n2 = b.length;
-        int n = n1 + n2;
-        
-        int ind2 = n / 2;
-        int ind1 = ind2 -1;
-        
-        int count = 0;
-        int i=0;
-        int j=0;
-        
-        int ele1 = -1;
-        int ele2 = -1;
-        
-        while(i<n1 && j<n2) {
-            if(a[i] < b[j]) {
-                if(count == ind1) ele1 = a[i];
-                if(count == ind2) ele2 = a[i];
-                count++ ;
-                i++ ;
+
+        if(n1 > n2) return medianOf2(b,a);
+        int n = n1+ n2;
+
+        int low = 0;
+        int high = n1;
+
+        int left = (n1 + n2 + 1) / 2;
+
+        while(low <= high) {
+            int mid1 = low + (high - low ) / 2;
+            int mid2 = left - mid1;
+
+            int l1 = Integer.MIN_VALUE , l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE , r2 = Integer.MAX_VALUE;
+
+            if(mid1 < n1) r1 = a[mid1];
+            if(mid2 < n2) r2 = b[mid2];
+            if(mid1 - 1 >= 0) l1 = a[mid1-1];
+            if(mid2 -1>= 0 ) l2 = b[mid2-1];
+
+            if(l1 <= r2 && l2 <= r1) {
+                if(n%2 ==1) return max(l1,l2);
+                return (max(l1,l2) + min(r1,r2)) / 2.0;
             }
-            else {
-                if(count == ind1) ele1 = b[j];
-                if(count == ind2) ele2 = b[j];
-                count++ ;
-                j++;
-            }
+            else if(l1 > r2) high = mid1-1;
+            else low = mid1+1;
         }
-        while(i < n1) {
-            if(count == ind1) ele1 = a[i];
-            if(count == ind2) ele2 = a[i];
-            count++ ;
-            i++ ;
-        }
-        
-        while( j< n2) {
-            if(count == ind1) ele1 = b[j];
-            if(count == ind2) ele2 = b[j];
-            count++ ;
-            j++ ;
-        }
-        
-        if( n%2 == 1) return ele2;
-        
-        return (ele1 + ele2) / 2.0;
+        return 0;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
