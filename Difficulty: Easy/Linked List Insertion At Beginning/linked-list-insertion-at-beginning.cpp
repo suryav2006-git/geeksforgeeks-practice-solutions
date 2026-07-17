fd116453ea6ad162e7bd57021b12fb7c27d266af
Surya V <mrstudieshelper@gmail.com>
2026-07-17 09:26:39 -0700

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Linked List Insertion At Beginning/linked-list-insertion-at-beginning.cpp
@@ -0,0 +1,26 @@
+/*
+class Node {
+  public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node *insertAtFront(Node *head, int x) {
+        // Code here
+        Node *temp = new Node(x);
+        
+        temp->next = head;
+        
+        head = temp;
+        
+        return head;
+    }
+};
